827e1912cd40c18fb996cd8c10c31cbd8a41ca28
jtahun <jtahun@bk.ru>
2026-06-29 23:06:49 +0500

    use dio.iterceptors in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:news/app.dart';
 import 'package:news/providers/articles_provider.dart';
+import 'package:news/providers/dio_articles_provider.dart';
 //import 'package:news/providers/faker_articles_provider.dart';
-import 'package:news/providers/http_articles_provider.dart';
+//import 'package:news/providers/http_articles_provider.dart';
 
 
 
 void main() {
-  GetIt.instance.registerSingleton<ArticlesProvider>(HttpArticlesProvider());
+  GetIt.instance.registerSingleton<ArticlesProvider>(DioArticlesProvider());
   runApp(const App());
 }
 
