6d2f9771bd92ce020bc891a1026cc4c228059942
jtahun <jtahun@bk.ru>
2026-06-29 21:58:39 +0500

    added handler of getAbyId

--- lib/providers/http_articles_provider.dart
+++ lib/providers/http_articles_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:news/models/article_data.dart';
 import 'package:news/models/comment_data.dart';
 import 'package:news/providers/articles_provider.dart';
@@ -5,26 +7,30 @@ import 'package:http/http.dart' as http;
 
 class HttpArticlesProvider implements ArticlesProvider{
   @override
-  Future<ArticleData?> getArticleById(int id) {
-    // TODO: implement getArticleById
-    throw UnimplementedError();
+  Future<ArticleData?> getArticleById(int id) async {
+    final uri = Uri.https('dummyjson.com','posts/$id');
+    final response = await http.get(uri);
+    final json = jsonDecode(response.body);
+    return ArticleData.fromJson(json);
   }
 
   @override
   Future<List<ArticleData>> getArticles() async {
     //final uri = Uri.parse('https://dummyjson.com/posts');
-    final uri = Uri.https('dummyjson.com','posts');
-    http.get(uri);
+    final uri = Uri.https('dummyjson.com','posts');  
     final response = await http.get(uri);
-    print(response.body);
-    return [];
     
+    final json = jsonDecode(response.body);
+    final list = json['posts'] as List;
+    
+    return list.map((postJson) => ArticleData.fromJson(postJson)).toList();
   }
 
   @override
-  Future<List<CommentData>> getComments(int articleId) {
+  Future<List<CommentData>> getComments(int articleId) async{
     // TODO: implement getComments
-    throw UnimplementedError();
+    //throw UnimplementedError();
+    return [];
   }
 
 }
